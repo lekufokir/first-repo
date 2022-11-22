a05b27b8c4175495eb5f3c7fc456c40fdc586778
lekufokir <lekufokrir@gmail.com>
2022-11-22 18:47:29 +0600

    javascript problem

---
+++ stringslice.js
@@ -0,0 +1,17 @@
+let newString = 'I Lovedvasdvsdfsd Bangladesh';
+let newString2 = '';
+
+    if(newString.length>=10){
+        for (let i=0 ; i<newString.length ;i++) {
+            if(i<3 || i>(newString.length-4)){
+                newString2 += newString[i];
+            }
+            else{
+                newString2 += '.';
+            }
+        }
+    }
+    else{
+        console.log('The string length is less than 10!');
+    }
+    console.log(newString2);
